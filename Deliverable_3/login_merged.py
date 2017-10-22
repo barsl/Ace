714135b66cee416807f493510097ecd40de74fbd
Diana Soto <dsoto@Charlotte-2.local>
2017-10-22 19:54:04 -0400

    working on the problem class for UI

--- Deliverable_3/login_merged.py
+++ Deliverable_3/login_merged.py
@@ -24,15 +24,15 @@ class AoS(tk.Tk):
         # the dictionary will contain the different screens
         self.frames = {}
         
-        frame = LoginScreen(container, self)
-        self.frames[LoginScreen] = frame
+        frame = Problems(container, self)
+        self.frames[Problems] = frame
         
         # with grid you can assign columns and rows to your
         # sticky determines (alignment + stretch) 
         # stretch the window to north south east or west (n,s,e,w)
         frame.grid(row=0, column=0, sticky="nsew")
         
-        self.show_frame(LoginScreen)
+        self.show_frame(Problems)
 
 
     def show_frame(self, cont):
@@ -58,9 +58,9 @@ class LoginScreen(tk.Frame):
         loginlbl = ttk.Label(self, text ="Please Log In: ",
                              font=APP_HIGHLIGHT_FONT)
         # create the username and password fields
-        username_label = tk.Label(self, text="Username", font=REGULAR_FONT)
+        username_label = tk.Label(self, text="Username", font=REGULAR_FONT, foreground="red")
         username_entry = tk.Entry(self, show="email address")
-        password_label = tk.Label(self, text="Password", font=REGULAR_FONT)
+        password_label = tk.Label(self, text="Password", font=REGULAR_FONT, foreground="red")
         # the show field of the password window makes sure that we only
         # show '*' when somebody types in the password
         password_entry = tk.Entry(self, show="*")
@@ -77,7 +77,37 @@ class LoginScreen(tk.Frame):
         password_label.pack()
         password_entry.pack()
         login_btn.pack(pady=20)        
+
+class Problems(tk.Frame):
+    '''Creates a prob screen, which will be the 
+    first screen of our Application'''
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.init_window()
         
+    def init_window(self):
+        '''Initialises the GUI window and its elements
+        Sets the different widgets that will be on the screen '''
+        # login text
+        addproblemlbl = ttk.Label(self, text ="Add a problem to the Question Bank",
+                             font=APP_HIGHLIGHT_FONT)
+        removeproblemlbl = ttk.Label(self, text ="Remove a problem from the Question Bank ",
+                             font=APP_HIGHLIGHT_FONT)
+        # create the username and password fields
+        addproblem_label = tk.Label(self, text="Question", font=REGULAR_FONT, foreground="red")
+        problem_entry = tk.Entry(self)
+        
+        add_btn = ttk.Button(text="Add")
+        remove_btn = ttk.Button(text="Remove")
+        # empty label to create some space between the top 
+        # the entry labels
+        empty_label = tk.Label(self, text="\n").pack()
+        # place our created label inside the
+
+        addproblem_label.pack()
+        problem_entry.pack()
+        add_btn.pack(pady=20)
+        remove_btn.pack(pady=20)
         
 if __name__ == "__main__":
     app = AoS()
