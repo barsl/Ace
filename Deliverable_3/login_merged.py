5fed3168255036d74a6fc054ccbc66063dcfcf9d
gsteinb <gerrit_steinbach@hotmail.com>
2017-10-22 22:26:33 -0400

    Added some new screens, and cleaned up code

--- Deliverable_3/login_merged.py
+++ Deliverable_3/login_merged.py
@@ -5,6 +5,7 @@ from tkinter import font
 APP_HIGHLIGHT_FONT = ("Helvetica", 14, "bold")
 REGULAR_FONT = ("Helvetica", 12, "normal")
 
+
 class AoS(tk.Tk):
     '''Class that contains everything in the Application '''
     def __init__(self, *args, **kwargs):
@@ -23,11 +24,11 @@ class AoS(tk.Tk):
         # the frame that is on the top is the one that is on the screen
         # the dictionary will contain the different screens
         self.frames = {}
-    
+        
         # this loop adds screens to the dictionary,
         # once we build more screens, add them to the tuple 
         # for F in TUPLE e.g. (LoginScreen, HomeScreen, DataBlaBLa)
-        for F in (LoginScreen, Problems):
+        for F in (LoginScreen, HomeScreen):
             # here F is the name of the Screen
             frame = F(container, self)
             self.frames[F] = frame
@@ -38,6 +39,21 @@ class AoS(tk.Tk):
             
         self.show_frame(LoginScreen)
 
+    def login(self, num):
+        '''Function that continues to the next screen depending on if the
+        login is valid. This function will take 1 as valid, and 0 as invalid.
+        would work with boolean variables as well
+        login(1)
+        >>>(GUI opens HomeScreen)
+        login(0)
+        >>>Invalid Username or Password
+        '''
+        if (num == 0):
+            # call the showframe function 
+            self.show_frame(HomeScreen)
+        else:
+            print("Invalid username or password")
+
 
     def show_frame(self, cont):
         ''' function that determines which of the screens will be viewed by
@@ -46,7 +62,7 @@ class AoS(tk.Tk):
         @param cont - name of the screen that needs to be displayed
         this is stored in the frames dictionary in self'''
         # get the frame from the dictionary
-        frame = self.frames[cont]   
+        frame = self.frames[cont]
         frame.tkraise()
 
 
@@ -55,90 +71,85 @@ class LoginScreen(tk.Frame):
     first screen of our Application'''
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.controller = controller
+        self.credentials = {'Username' : '','Password': ''}
         self.init_window(controller)
+
         
-    def init_window(self, controller):
-        '''Initialises the GUI window and its elements
-        Sets the different widgets that will be on the screen '''
+    def create_login(self, controller):
+        '''creates login button'''
+        button = ttk.Button(self)
+        button["text"] = "Login"
+        button["command"] = lambda : controller.login(0)
+        button.pack(pady=20)
+    
+    def create_entries(self):
+        '''creates the entry fields for username and password'''
         # login text
         loginlbl = ttk.Label(self, text ="Please Log In: ",
-                             font=APP_HIGHLIGHT_FONT)
+                             font=APP_HIGHLIGHT_FONT)        
         # create the username and password fields
-        username_label = tk.Label(self, text="Username", font=REGULAR_FONT, foreground="red")
-        username_entry = tk.Entry(self, show="email address")
-        password_label = tk.Label(self, text="Password", font=REGULAR_FONT, foreground="red")
+        username_label = tk.Label(self, text="Username", font=REGULAR_FONT)
+        username_entry = tk.Entry(self)
+        password_label = tk.Label(self, text="Password", font=REGULAR_FONT)
         # the show field of the password window makes sure that we only
         # show '*' when somebody types in the password
         password_entry = tk.Entry(self, show="*")
-        
-        login_btn = tk.Button(self, text="Log in", command=lambda: controller.show_frame(Problems))        
-        # empty label to create some space between the top 
-        # the entry labels
-        empty_label = tk.Label(self, text="\n").pack()
-        # place our created label inside the
         loginlbl.pack()
         username_label.pack()
         username_entry.pack()
         password_label.pack()
-        password_entry.pack()
-        login_btn.pack(pady=20)        
-
-class Problems(tk.Frame):
-    '''Creates a prob screen, which will used by admin to add/edit and remove problems from '''
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.init_window(controller)
-        
+        password_entry.pack()      
+    
     def init_window(self, controller):
         '''Initialises the GUI window and its elements
-        Sets the different widgets that will be on the screen '''
- 
-        # create the question label
-        addproblem_label = tk.Label(self, text="Please select from the options below: ", font=REGULAR_FONT, foreground="red")
-        problem_entry = tk.Entry(self)
-        
-        add_btn = ttk.Button(text="Add")
-        remove_btn = tk.Button(self, text="Remove")
+        Sets the different widgets that will be on the screen '''        
         # empty label to create some space between the top 
         # the entry labels
         empty_label = tk.Label(self, text="\n").pack()
         # place our created label inside the
+        self.create_entries()
+        self.create_login(controller)
 
-        addproblem_label.pack()
-        problem_entry.pack()
-        add_btn.pack(pady=20)
-        remove_btn.pack(pady=20)
 
-class AddProblems(tk.Frame):
-    '''Creates a prob screen, which will used by admin to add/edit and remove problems from '''
+class HomeScreen(tk.Frame):
+    ''' Homescreen that appears after the user logs in
+    at the moment the homescreen is just a placeholder for some buttons'''
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.init_window(controller)
+    
+    def create_logout_button(self, controller):
+        ''' creates logout button'''
+        # button will go to login screen
+        logout_btn = ttk.Button(self)
+        logout_btn["text"] = "Logout"
+        logout_btn["command"] = lambda : controller.show_frame(LoginScreen)
+        logout_btn.pack()
+    
+    def create_edit_problem_button(self):
+        ''' creates edit problem button'''
+        button = ttk.Button(self)
+        button["text"] = "Edit Problems"
+       # button["command"] = lambda : controller.showframe(EditProblemScreen)
+        button.pack()
         
+    def create_add_user_button(self):
+        ''' creates add user button'''
+        button = ttk.Button(self)
+        button["text"] = "Add User"
+       # button["command"] = lambda : controller.showframe(AddUserScreen)
+        button.pack()
+    
     def init_window(self, controller):
-        '''Initialises the GUI window and its elements
-        Sets the different widgets that will be on the screen '''
-        # login text
-        addproblemlbl = ttk.Label(self, text ="Add a problem to the Question Bank",
-                             font=APP_HIGHLIGHT_FONT)
-        removeproblemlbl = ttk.Label(self, text ="Remove a problem from the Question Bank ",
-                             font=APP_HIGHLIGHT_FONT)
-        # create the question label
-        addproblem_label = tk.Label(self, text="Please enter a new question", font=REGULAR_FONT, foreground="red")
-        problem_entry = tk.Entry(self)
+        ''' initialises the homescreen and its elements'''
+        homescreen_label = ttk.Label(self, text="Home", font=APP_HIGHLIGHT_FONT)
+        # just to get the formatting correct
+        empty_label = ttk.Label(self, text="\n").pack()
+        homescreen_label.pack()
+        self.create_add_user_button()
+        self.create_edit_problem_button()
+        self.create_logout_button(controller)
         
-        add_btn = ttk.Button(text="Add")
-        remove_btn = tk.Button(self, text="Remove")
-        # empty label to create some space between the top 
-        # the entry labels
-        empty_label = tk.Label(self, text="\n").pack()
-        # place our created label inside the
-
-        addproblem_label.pack()
-        problem_entry.pack()
-        add_btn.pack(pady=20)
-        remove_btn.pack(pady=20)
         
 if __name__ == "__main__":
     app = AoS()
