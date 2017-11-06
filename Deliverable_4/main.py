1522262641f8d3285bef04e329473aa336b8d97e
Diana Soto <dsoto@Charlotte-2.local>
2017-11-05 23:25:41 -0500

    create the View Assignments GUI, edited other interfaces also

--- Deliverable_4/main.py
+++ Deliverable_4/main.py
@@ -6,7 +6,9 @@ import database_api as db
 
 APP_HIGHLIGHT_FONT = ("Helvetica", 14, "bold")
 REGULAR_FONT = ("Helvetica", 12, "normal")
+TITLE_FONT = ("Helvetica", 14, "normal")
 NICE_BLUE = "#3399FF"
+HOME_FONT = ("Comic Sans", 26, "bold")
 
 class GUISkeleton(ttk.Frame):
     '''Skeleton for creating frames in Tkinter'''
@@ -81,7 +83,7 @@ class AoS(tk.Tk):
         # once we build more screens, add them to the tuple 
         # for F in TUPLE e.g. (LoginScreen, HomeScreen, DataBlaBLa)
         for F in (LoginScreen, HomeScreen, ProblemInterface, UserHome,
-                  UserInterface):
+                  UserInterface, AssignmentsInterface):
             # here F is the name of the Screen
             frame = F(self.container, self)
             self.frames[F] = frame
@@ -173,7 +175,7 @@ class LoginScreen(GUISkeleton):
                     showinfo("Fail", "User has no role")
             else :
                 # otherwise pop msg to terminal
-                showinfo("Fail", "Wrong combo")
+                showinfo("Fail", "We do not have that user information. Please try again!")
             self.entry_fields["Email"].set('')
             self.entry_fields["Password"].set('')
         # print msg to terminal if email doesnt exist
@@ -189,14 +191,20 @@ class UserHome(GUISkeleton):
         
     def init_window(self, controller):
         '''initialises the window'''
-        self.create_empty_label(5)
+        homescreen_label = self.create_label(self, "Home", HOME_FONT, "blue")
+        self.create_empty_label(1)
+        homescreen_label.pack()
+        self.create_empty_label(2)
         for button in self.buttons:
             new_button = self.create_button(self, button)
-            if (button == "Logout"):
+            if button == "View Assignments":
+                new_button["command"] = lambda : controller.show_frame(AssignmentsInterface)
+            elif (button == "Logout"):
                 new_button["command"] = (lambda :
                                          controller.show_frame(LoginScreen))
             new_button.pack()
 
+       
 class HomeScreen(GUISkeleton):
     ''' Homescreen that appears after the user logs in
     at the moment the homescreen is just a placeholder for some buttons'''
@@ -221,11 +229,12 @@ class HomeScreen(GUISkeleton):
     
     def init_window(self, controller):
         ''' initialises the homescreen and its elements'''
-        homescreen_label = self.create_label(self, "Home", APP_HIGHLIGHT_FONT)
+        homescreen_label = self.create_label(self, "Home", HOME_FONT, "blue")
         # just to get the formatting correct
         # empty_label = ttk.Label(self, text="\n").pack()
         self.create_empty_label(1)
         homescreen_label.pack()
+        self.create_empty_label(2)
         self.create_buttons(controller)
         
 
@@ -270,8 +279,8 @@ class UserInterface(GUISkeleton):
         self.labels = ["Role", "Name", "Email"]
         # label at top of the frame
         new_label = self.create_label(self, "User Database Management\n",
-                                      REGULAR_FONT,
-                                      "Green").grid(row=0, column=1) 
+                                      TITLE_FONT,
+                                      "Red").grid(row=0, column=1,pady=10) 
         # dictionaries to contain the widgets and associate widget to
         # corresponding user id
         self.roles = {}
@@ -299,7 +308,7 @@ class UserInterface(GUISkeleton):
         add_user_button.grid(row=2, column=3)
         back_button = self.create_button(self, "Back")
         back_button["command"] = lambda : controller.show_frame(HomeScreen)
-        back_button.grid(row=2, column=4)
+        back_button.grid(row=0, column=3)
         # generate all the dynamically generaterd widget rows
         self.gen_rows()
         
@@ -460,8 +469,8 @@ class ProblemInterface(GUISkeleton):
         self.labels = ["Subject", "Question", "Answer"]
         # label at top of the frame
         title = self.create_label(self, "Problem Database Management\n",
-                                  REGULAR_FONT, "Green").grid(
-                                 row=0, column=1)
+                                  TITLE_FONT,
+                                  "Red").grid(row=0, column=1, pady=10)
         # dictionaries to contain the widgets and associate widget to
         # correspondin problem id
         self.subjects = {}
@@ -488,7 +497,7 @@ class ProblemInterface(GUISkeleton):
         # set button method to add_problem
         add_problem_button.config(command=lambda : self.add_problem())
         back_button["command"] = lambda: controller.show_frame(HomeScreen)
-        back_button.grid(row=2, column=4)
+        back_button.grid(row=0, column=3)
         
         # generate all the dynamically generated widget rows
         self.gen_rows()
@@ -611,6 +620,120 @@ class ProblemInterface(GUISkeleton):
             self.updates[qid].config(command=lambda j=qid: self.up_problem(j))         
 
 
+class Assignment():
+    '''
+    A problem object which is used to interact with assignment's data,
+    and perform actions that affect assignment's data
+    '''
+    def __init__(self,aid):
+        '''
+        aid is the assignment id of the assignment we want to create
+        '''
+        # get user details from database
+        assignment = db.get_assignment_details(conn, aid)[0]
+        # assign corresponding values to variables
+        self.aid = assignment[0]
+        self.topic = assignment[1]
+        self.deadline = assignment[2]
+        self.visible = assignment[3]
+        self.questions = assignment[4]
+        self.length = assignment[5]
+        
+    # getters and setters
+    def get_aid(self):
+        return self.aid
+    def get_deadline(self):
+        return self.deadline
+    def get_length(self):
+        return self.length     
+    def get_topic(self):
+        return self.topic
+    def get_questions(self):
+        return self.questions
+    def get_visible(self):
+        return self.visible
+
+class AssignmentsInterface(GUISkeleton):
+    '''
+    Objects of this type are used to generate the GUI for the user to see all Assignments screen
+    '''
+    def __init__(self, parent, controller):
+        GUISkeleton.__init__(self, parent)        
+        self.labels = ["Topic", "Deadline", "Grade"]
+        # label at top of the frame
+        title = self.create_label(self, "Assignments\n",
+                                  TITLE_FONT,
+                                  "Red").grid(row=1, column=1)
+        # dictionaries to contain the widgets and associate widget to
+        # correspondin assignment id
+        self.names = {}
+        self.deadlines = {}
+        self.grades = {}
+        # the buttons
+        self.past_attempts = {}
+        self.new_attempts = {}
+
+        i = 0
+        for label in self.labels:
+            new_label = self.create_label(self, label, REGULAR_FONT,
+                                          NICE_BLUE).grid(row=2, column=i)
+            i+=1
+        back_button = self.create_button(self, "Back")
+        # set button method to go back
+        back_button = self.create_button(self, "Back")
+        back_button["command"] = lambda: controller.show_frame(HomeScreen)
+        back_button.grid(row=1, column=4)
+        
+        # generate all the dynamically generated widget rows
+        self.gen_rows()
+        
+        # enable clicking functionality for all the buttons
+        #self.enable_buttons()
+
+    '''
+    def enable_buttons(self):
+        # get a list of all existing problem ids
+        assignment_ids = db.get_assignment_ids(conn)        
+        # configure clicking function for all the delete buttons
+        for aid in assignment_ids:
+            self.past_attempts[aid].config(command=lambda j=qid: self.del_problem(j))
+        # configure clicking function for all the update buttons
+        for qid in problem_ids:
+            self.updates[qid].config(command=lambda j=qid: self.up_problem(j))
+    '''
+    def gen_rows(self):
+        # get a list of all the user ids in the database
+        #ids = db.get_assignment_ids(conn)
+        ids = [5,9,10]
+        # set iterator for grid rows
+        i = 0
+        # for each id create a row
+        for aid in ids:
+            # create new entries 
+            name_entry = ttk.Entry(self, font=REGULAR_FONT)
+            deadline_entry = ttk.Entry(self, font=REGULAR_FONT)
+            grade_entry = ttk.Entry(self, font=REGULAR_FONT)
+            # add to corresponding dictonaries with user ids as keys
+            self.names[aid] = name_entry
+            self.deadlines[aid] = deadline_entry    
+            self.grades[aid] = grade_entry
+          
+            # create new buttons
+            past_attempt_button = self.create_button(self, "Past Attempts")
+            new_attempt_button = self.create_button(self, "New Attempt")
+            # add to corresponding dictonaries with user ids as keys        
+            self.past_attempts[aid] = past_attempt_button
+            self.new_attempts[aid] = new_attempt_button
+            
+            # set everything nicely on the grid using an iterator i
+            name_entry.grid(row=i+3, column=0)
+            deadline_entry.grid(row=i+3, column=1)
+            grade_entry.grid(row=i+3, column=2)
+            new_attempt_button.grid(row=i+3, column=3)
+            past_attempt_button.grid(row=i+3, column=4)
+            i += 1
+            
+            
 if __name__ == "__main__":
     conn = db.sqlite3.connect('ace.db')
     app = AoS()
